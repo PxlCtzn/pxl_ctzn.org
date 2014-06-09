311c1683be3ea6a22c9e62e83dbfa4b35981887d
blegrand <mfib4c22>
2014-06-09 18:18:04 +0200

    Home hexagon menu 0.1
    Very first version of the HexaHome menu.

--- src/PxlCtzn/HomeBundle/Menu/MenuBuilder.php
+++ src/PxlCtzn/HomeBundle/Menu/MenuBuilder.php
@@ -7,25 +7,34 @@ use Symfony\Component\DependencyInjection\ContainerAware;
 
 class MenuBuilder extends ContainerAware
 {
+
+    /**
+     * @param FactoryInterface $factory
+     * @param array $options
+     * @return \Knp\Menu\ItemInterface
+     */
     public function homeMenu(FactoryInterface $factory, array $options){
 		$menu = $factory->createItem('root');
 		$menu->setChildrenAttribute('class', 'nav');
-		
+
+        $menu->addChild('Pxl Ctzn', array('route' => 'blog_homepage'))
+             ->setAttribute('id', 'brand');
+
 		$menu->addChild('Blog', array('route' => 'blog_homepage'))
-			 ->setAttribute('icon', 'icon-file-text-o')
-			 ->setAttribute('color','red');
+             ->setAttribute('class', 'menu-item')
+			 ->setAttribute('icon', 'fa-file-text-o');
 			 
 		$menu->addChild('Projects', array('route' => 'blog_homepage'))
-			 ->setAttribute('icon', 'icon-wrench')
-			 ->setAttribute('color','green');
+             ->setAttribute('class', 'menu-item')
+			 ->setAttribute('icon', 'fa-wrench');
 			 
 		$menu->addChild('Git', array('route' => 'blog_homepage'))
-			 ->setAttribute('icon', 'icon-github')
-			 ->setAttribute('color','orange');
+             ->setAttribute('class', 'menu-item')
+			 ->setAttribute('icon', 'fa-github');
 			 
 		$menu->addChild('Calendar', array('route' => 'blog_homepage'))
-			 ->setAttribute('icon', 'icon-calendar-o')
-			 ->setAttribute('color','pink');
+            ->setAttribute('class', 'menu-item')
+			 ->setAttribute('icon', 'fa-calendar-o');
 			 
 		return $menu;
 	}
